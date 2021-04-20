367bbeef707a555be84e37f7d8f97cda6c1fdddc
Curtis Conrad <conradc5749@yahoo.com64824279+ConradC2@users.noreply.github.com>
2021-04-20 16:53:38 -0400

    Conrad updates seeding and joined table seeding

--- server/db/seeds/initial_data.js
+++ server/db/seeds/initial_data.js
@@ -1,16 +1,23 @@
+/* eslint-disable no-await-in-loop, no-use-before-define, no-restricted-syntax */
+
+const _ = require('lodash');
 const faker = require('faker');
 
 let LOCATION_SEED_COUNT = 10;
+let UTC_SEED_COUNT = 100;
 
-exports.seed = async function(knex) {
+exports.seed = async function seed(knex) {
   await seedLocations(knex);
+  await seedUtc(knex);
+  const locationsIds = await knex('locations').pluck('id');
+  const utcsIds = await knex('utcs').pluck('id');
+  await seedLocationsUtcs(knex, locationsIds, utcsIds);
 };
 
 async function seedLocations(knex) {
-  await knex('locations').del();
+  await knex('locations').truncate();
 
-  while (LOCATION_SEED_COUNT-- > 0) {
-    // eslint-disable-next-line no-await-in-loop
+  while (LOCATION_SEED_COUNT > 0) {
     await knex('locations').insert({
       name: `Fort ${faker.unique(faker.name.lastName)}`,
       country: faker.address.countryCode(),
@@ -19,5 +26,37 @@ async function seedLocations(knex) {
         faker.address.longitude(),
       ]),
     });
+    LOCATION_SEED_COUNT -= 1;
+  }
+}
+
+async function seedUtc(knex) {
+  await knex('utcs').truncate();
+
+  while (UTC_SEED_COUNT > 0) {
+    await knex('utcs').insert({
+      unit_type_code: faker.unique(faker.random.alphaNumeric, [5]),
+      category: faker.random.arrayElement(['Support', 'Equipment Only', 'BOSI', 'Mission']),
+      nomenclature: faker.company.catchPhrase(),
+      description: faker.company.bs(),
+    });
+    UTC_SEED_COUNT -= 1;
+  }
+}
+
+async function seedLocationsUtcs(knex, allLocationIds, allUtcsIds) {
+  const possibleUtcOptions = _.range(1, 11);
+
+  await knex('locations_utcs').truncate();
+
+  for (const locationId of allLocationIds) {
+    const numberOfUtcs = _.sample(possibleUtcOptions);
+    const randomUtcIds = _.sampleSize(allUtcsIds, numberOfUtcs);
+    for (const randomUtcId of randomUtcIds) {
+      await knex('locations_utcs').insert({
+        location_id: locationId,
+        utc_id: randomUtcId,
+      });
+    }
   }
 }
