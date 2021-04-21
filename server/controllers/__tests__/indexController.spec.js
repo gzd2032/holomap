3ea7ff44484881c3ff54e6fe8628581e3281c0cd
Gordon Deng <gzd2032@gmail.com>
2021-04-21 16:55:50 +0200

    deleted coverage folder and create services controller

--- server/controllers/__tests__/indexController.spec.js
+++ server/controllers/__tests__/indexController.spec.js
@@ -1,7 +1,7 @@
 /* global describe, beforeEach, afterEach, it, expect */
 jest.mock('../../db/db');
 const IndexController = require('../indexController');
-const knex = require('../../db/db');
+const db = require('../../db/db');
 
 describe('The index controller', () => {
   let indexController = null;
@@ -19,16 +19,16 @@ describe('The index controller', () => {
     description: 'streamline cutting-edge solutions',
   };
   beforeEach(async () => {
-    await knex.insert(mockLocation).into('locations');
-    await knex.insert(mockUTC).into('utcs');
-    indexController = IndexController(knex);
+    await db.insert(mockLocation).into('locations');
+    await db.insert(mockUTC).into('utcs');
+    indexController = IndexController(db);
   });
 
   afterEach(() => {
-    knex.del();
+    db.del();
   });
 
   test('knex.select should be a mock function', () => {
-    expect(typeof knex.select).toBe('function');
+    expect(typeof db.select).toBe('function');
   });
 });
