c9057d72eecc32a31cdb602cb9c965d12dc37547
Dustin Stiles <duwstiles@gmail.com>
2021-04-22 11:20:11 -0400

    make eslint happy and ignore coverage

--- server/api/services/UTCService.js
+++ server/api/services/UTCService.js
@@ -10,12 +10,12 @@ module.exports = (db) => ({
       .first('*')
       .from('utcs')
       .where('id', id);
+
+    if (!utc) return undefined;
+
     const locations = await db.select().from('locations').join('locations_utcs', 'locations_utcs.location_id', 'locations.id').where('locations_utcs.utc_id', id);
-    if (utc) {
-      utc['locations'] = locations;
-      return utc;
-    }
-    return utc;
+
+    return Object.assign(utc, { locations });
   },
 
   async createUTC(newUTC) {
